ace5f00f927d757f80a1ef07c1cd97a345e718b9
phanto777 <saykechannel@gmail.com>
2025-02-28 18:12:41 +0100

    Update door.js

--- door.js
+++ door.js
@@ -1,3 +1,16 @@
-var script = document.createElement('script');
-script.src = 'https://phanto777.github.io/upgradeslither/';
-document.body.appendChild(script);
+(function() {
+    console.log("🔥 Slither.io Hack Activé !");
+
+    // Vérifie si le jeu est chargé
+    function waitForSnake() {
+        if (window.snake) {
+            console.log("🐍 Serpent détecté !");
+            window.snake.sc = 5000; // Définit ta taille à 5000 (change la valeur si besoin)
+        } else {
+            console.log("⏳ En attente du serpent...");
+            setTimeout(waitForSnake, 1000);
+        }
+    }
+
+    waitForSnake(); // Démarre la vérification
+})();
